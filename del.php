6f984303ef17cb97df2481537af434b98ea0363f
abdesslemdhaker <abdesslemdhakerr@gmail.com>
2023-11-22 17:47:02 +0100

    add comment

--- del.php
+++ del.php
@@ -1,6 +1,6 @@
 <?php
 
-include "/assets/database/db.php";
+include "./assets/database/db.php";
 
 if (isset($_POST) && $_POST) {
     $name = $_POST['name'];
@@ -17,11 +17,9 @@ $result = mysqli_query($db, $sql);
   <title>Delete produit | campi</title>
   <meta charset="utf-8">
   <meta name="viewport" content="width=device-width, initial-scale=1">
-  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/css/bootstrap.min.css">
-  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.6.4/jquery.min.js"></script>
-  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/js/bootstrap.min.js"></script>
 </head>
 <body>
+<?php include "./navbar.php";?>
 
 <div class="container">
   <h2>Delete un produit</h2>
--- register.php
+++ register.php
@@ -2,28 +2,20 @@
 session_start();
 error_reporting(0);
 ini_set("display_errors", 0);
-include "assets/database/db.php";
+include "./assets/database/db.php";
+
 if (isset($_POST) && $_POST) {
   $name = $_POST['nom'];
   $prenom = $_POST['prenom'];
   $username = $_POST['username'];
   $telephone = $_POST['telephone'];
   $email = $_POST['email'];
-  $password = $_POST['password'];
-  $sql = "INSERT INTO `users`(`username`, `nom`, `prenom`, `email`, `password`, `telephone`) VALUES (".$username.",".$name.",".$prenom.",".$email.",".$password.",".$telephone.")"
-  $result = mysqli_query($db, $sql);
-  if ($result) {
-    $_SESSION['id'] = $res['username'];
-  }else{
-  $err = "true";
-  } 
+  $Password = $_POST['Password'];
+    $sql = "INSERT INTO `users`(`username`, `nom`, `prenom`, `email`, `password`, `telephone`) VALUES ('".$username."','".$name."','".$prenom."','".$email."','".$password."','".$telephone."')";
+      $result = mysqli_query($db, $sql);
   }
-$sql = "SELECT * FROM produits";
-$result = mysqli_query($db, $sql);
-$categoriessql = "SELECT * FROM categories";
-$categories = mysqli_query($db, $categoriessql);
-?>
 
+?>
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -32,195 +24,14 @@ $categories = mysqli_query($db, $categoriessql);
   <title>
       Campi.tn - the most comprehensive resource for beautiful private campsites.
     </title>
-    <link rel="icon" type="image/png" href="/assets/images/logo.png" />
-    <link href="/assets/css/style.css" rel="stylesheet">
-  <link href="//maxcdn.bootstrapcdn.com/bootstrap/3.3.0/css/bootstrap.min.css" rel="stylesheet">
-<script src="//maxcdn.bootstrapcdn.com/bootstrap/3.3.0/js/bootstrap.min.js"></script>
-<script src="//code.jquery.com/jquery-1.11.1.min.js"></script>
-<script src="https://use.fontawesome.com/07b0ce5d10.js"></script>
 
 </head>
 <body>
-<nav class="topBar">
-    <div class="container">
-      <ul class="topBarNav pull-right">
-        <li class="dropdown">
-          <a href="#" class="dropdown-toggle" data-toggle="dropdown" data-hover="dropdown" data-close-others="false"> <i class="fa fa-money mr-5"></i>TND<i class="fa fa-angle-down ml-5"></i>
-          </a>
-          <ul class="dropdown-menu w-100" role="menu">
-            <li><a href="#"><i class="fa fa-money mr-5"></i>TND</a>
-            </li>
-            <li class=""><a href="#"><i class="fa fa-eur mr-5"></i>EUR</a>
-            </li>
-            <li><a href="#"><i class="fa fa-usd mr-5"></i>USD</a>
-            </li>
-          </ul>
-        </li>
+<?php include "./navbar.php";?>
 
-        <li class="dropdown">
-          <a href="#" class="dropdown-toggle" data-toggle="dropdown" data-hover="dropdown" data-close-others="false"> <i class="fa fa-user mr-5"></i><span class="hidden-xs">My Account<i class="fa fa-angle-down ml-5"></i></span> </a>
-          <ul class="dropdown-menu w-150" role="menu">
-          <?php if (isset($_SESSION["id"]) && $_SESSION["id"]) {
-              echo '<li><a href="logout.php">LogOut</a>
-            </li>';
-            header("Location: /");
-          } else {
-              echo '<li><a href="login.php">Login</a>
-            </li>';
-              echo '<li><a href="register.php">Create Account</a>
-            </li>';
-          } ?>
-          
-            <li class="divider"></li>
-            </li>
-            <li><a>My Cart</a>
-            </li>
-            <li><a>Checkout</a>
-            </li>
-          </ul>
-        </li>
-        <li class="dropdown">
-          <a href="#" class="dropdown-toggle" data-toggle="dropdown" data-hover="dropdown" data-close-others="false"> <i class="fa fa-shopping-basket mr-5"></i> <span class="hidden-xs">
-                                Cart<sup class="text-primary">(0)</sup>
-                                <i class="fa fa-angle-down ml-5"></i>
-                            </span> </a>
-          <ul class="dropdown-menu cart w-250" role="menu">
-            <li>
-              <div class="cart-items">
-                <ol class="items">
-                <!--
-                    <li>
-                    <a href="#" class="product-image"> <img src="" class="img-responsive" alt="Sample Product"> </a>
-                    <div class="product-details">
-                      <div class="close-icon"> <a href="#"><i class="fa fa-close"></i></a> </div>
-                      <p class="product-name"> <a href="#">Lorem Ipsum dolor sit</a> </p> <strong>1</strong> x <span class="price text-primary">$29.99</span> </div>
-                  </li>
-                  -->
-                </ol>
-              </div>
-            </li>
-            <li>
-              <div class="cart-footer"> <a href="#" class="pull-left"><i class="fa fa-cart-plus mr-5"></i>View
-					Cart</a> <a href="#" class="pull-right"><i class="fa fa-shopping-basket mr-5"></i>Checkout</a> </div>
-            </li>
-          </ul>
-        </li>
-      </ul>
-    </div>
-  </nav>
-  <div class="middleBar">
-    <div class="container">
-  <div class="row display-table">
-    <div class="col-sm-3 vertical-align text-left hidden-xs">
-      <a href="/"> <img width="" src="/assets/images/logo.png" alt="campi"></a>
-    </div>
-    <div class="col-sm-7 vertical-align text-center">
-      <form>
-        <div class="row grid-space-1">
-          <div class="col-sm-6">
-            <input type="text" name="keyword" class="form-control input-lg" placeholder="Search">
-         </div>
-          <div class="col-sm-3">
-            <select class="form-control input-lg" name="category">
-              <option value="all">All Categories</option>
-              <?php
-              if (mysqli_num_rows($categories) > 0) {
-        while ($categoriesres = mysqli_fetch_assoc($categories)) { ?>
-              <option value="<?= $categoriesres["id"] ?>"><?= $categoriesres["name"] ?></option>
-              <?php }
-    } ?>
-            </select>
-          </div>
-          <div class="col-sm-3">
-            <input type="submit" class="btn btn-default btn-block btn-lg" value="Search">
-         </div>
-        </div>
-      </form>
-    </div>
-  </div>
-</div>
-</div>
-    
-<nav class="navbar navbar-main navbar-default" role="navigation" style="opacity: 1;">
-          <div class="container">
-            <div class="navbar-header">
-              <button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".navbar-1">
-                <span class="sr-only">Toggle navigation</span>
-                <span class="icon-bar"></span>
-                <span class="icon-bar"></span>
-                <span class="icon-bar"></span>
-              </button>             
-            </div>
-        
-            <div class="collapse navbar-collapse navbar-1" style="margin-top: 0px;">            
-              <ul class="nav navbar-nav">
-                <li><a href="/" class="dropdown-toggle" data-toggle="dropdown" data-hover="dropdown" data-close-others="false">Home</a></li>  
-                <li class="dropdown">
-                  <a href="#" class="dropdown-toggle" data-toggle="dropdown" data-hover="dropdown" data-close-others="false">Page <i class="fa fa-angle-down ml-5"></i></a>
-                  <ul class="dropdown-menu dropdown-menu-left">
-                    <li><a href="about.php">About Us</a></li>
-                    <li><a href="register.php">Register</a></li>
-                    <li><a href="login.php">Login</a></li>
-                    <li><a href="passwordrecovery.php">Password Recovery</a></li>
-                  </ul>
-                </li>
-              
-              </ul>
-            </div>
-          </div>
-        </nav>
-
-  <script type="text/javascript">
-    ! function($, n, e) {
-      var o = $();
-      $.fn.dropdownHover = function(e) {
-        return "ontouchstart" in document ? this : (o = o.add(this.parent()), this.each(function() {
-          function t(e) {
-            o.find(":focus").blur(), h.instantlyCloseOthers === !0 && o.removeClass("open"), n.clearTimeout(c), i.addClass("open"), r.trigger(a)
-          }
-          var r = $(this),
-            i = r.parent(),
-            d = {
-              delay: 100,
-              instantlyCloseOthers: !0
-            },
-            s = {
-              delay: $(this).data("delay"),
-              instantlyCloseOthers: $(this).data("close-others")
-            },
-            a = "show.bs.dropdown",
-            u = "hide.bs.dropdown",
-            h = $.extend(!0, {}, d, e, s),
-            c;
-          i.hover(function(n) {
-            return i.hasClass("open") || r.is(n.target) ? void t(n) : !0
-          }, function() {
-            c = n.setTimeout(function() {
-              i.removeClass("open"), r.trigger(u)
-            }, h.delay)
-          }), r.hover(function(n) {
-            return i.hasClass("open") || i.is(n.target) ? void t(n) : !0
-          }), i.find(".dropdown-submenu").each(function() {
-            var e = $(this),
-              o;
-            e.hover(function() {
-              n.clearTimeout(o), e.children(".dropdown-menu").show(), e.siblings().children(".dropdown-menu").hide()
-            }, function() {
-              var t = e.children(".dropdown-menu");
-              o = n.setTimeout(function() {
-                t.hide()
-              }, h.delay)
-            })
-          })
-        }))
-      }, $(document).ready(function() {
-        $('[data-hover="dropdown"]').dropdownHover()
-      })
-    }(jQuery, this);
-  </script>
 
 <div class="container">    
-<form>
+<form method="post" action="register.php">
   <div class="form-group">
     <label for="nom">Nom</label>
     <input type="text" class="form-control" name="nom" id="nom" aria-describedby="nomHelp" placeholder="Enter nom">
@@ -243,8 +54,8 @@ $categories = mysqli_query($db, $categoriessql);
     <small id="emailHelp" class="form-text text-muted">We'll never share your email with anyone else.</small>
   </div>
   <div class="form-group">
-    <label for="paswword">Password</label>
-    <input type="password" class="form-control" name="paswword" id="paswword" placeholder="Password">
+    <label for="Password">Password</label>
+    <input type="Password" class="form-control" name="Password" id="Password" placeholder="Password">
   </div>
   <div class="form-group form-check">
     <input type="checkbox" class="form-check-input" id="exampleCheck1">
