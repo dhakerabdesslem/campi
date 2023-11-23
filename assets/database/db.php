9035e60580997d1c213e7136e30b576e92306444
abdesslemdhaker <abdesslemdhakerr@gmail.com>
2023-11-23 13:47:36 +0100

    changment de db method

--- assets/database/db.php
+++ assets/database/db.php
@@ -3,7 +3,5 @@ $hostname = "sql208.infinityfree.com";
 $username = "if0_35387742";
 $password = "aBRoLPQ5wvC0o";
 $dbname = "if0_35387742_campi";
-
 $db = mysqli_connect($hostname, $username, $password,$dbname);
-
 ?>
