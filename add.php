e41031881cc984f2bbd221ce228771053ac9972d
abdesslemdhaker <abdesslemdhakerr@gmail.com>
2023-11-28 14:19:05 +0100

    refrech

--- add.php
+++ add.php
@@ -59,7 +59,9 @@ if (isset($_POST) && $_POST) {
   </div>
   <div class="form-group">
   <label for="categorie" aria-describedby="categorieHelp">Select categorie :</label>
-    <select class="form-control" id="categorie" name="categorie" required>  
+    <select class="form-control" id="categorie" name="categorie" required>
+    <option value="0" disabled selected>Select categorie</option>
+  
     <?php
               $categoriessql = "SELECT * FROM categories";
               $categories = mysqli_query($db, $categoriessql);
