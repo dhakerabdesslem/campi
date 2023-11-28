5415a46b66746f7780f433c264b378360c827a17
abdesslemdhaker <abdesslemdhakerr@gmail.com>
2023-11-28 15:26:24 +0100

    fix

--- index.php
+++ index.php
@@ -34,9 +34,9 @@ $result = mysqli_query($db, $sql);
 <section class="py-5">
             <div class="container px-4 px-lg-5 mt-5">
                 <div class="row gx-4 gx-lg-5 row-cols-2 row-cols-md-3 row-cols-xl-4 justify-content-center">
-                    <div class="col mb-5">
-                    <?php if (mysqli_num_rows($result) > 0) {
+                <?php if (mysqli_num_rows($result) > 0) {
         while ($res = mysqli_fetch_assoc($result)) { ?>
+                    <div class="col mb-5">
                         <div class="card h-100">
                             <div class="badge bg-dark text-white position-absolute" style="top: 0.5rem; right: 0.5rem">Sale</div>
                             <img class="card-img-top" src="<?= $res["image"] ?>" alt="<?= $res["name"] ?>" />
@@ -55,9 +55,11 @@ $result = mysqli_query($db, $sql);
                                 </div>
                             </div>
                         </div>
-                        <?php }
-    } ?>
                     </div>
+                    <?php }
+    } ?>
+    </div>
+</div>
 
         </section>
 </body>
