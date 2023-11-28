210b6a6b1f367a777dee8ed30e3c4f73acfce5b7
abdesslemdhaker <abdesslemdhakerr@gmail.com>
2023-11-28 14:56:02 +0100

    modifyy

--- index.php
+++ index.php
@@ -26,7 +26,7 @@ $result = mysqli_query($db, $sql);
   <title>
       Campi.tn - the most comprehensive resource for beautiful private campsites.
     </title>
-    <link href="./assets/css/css3.css" rel="stylesheet">
+    <link href="./assets/css/serv.css" rel="stylesheet">
 
 </head>
 <body>
