1361c51268c41e309d49b89913441938a436f1dd
abdesslemdhaker <abdesslemdhakerr@gmail.com>
2023-11-29 15:06:55 +0100

    fix

--- add.php
+++ add.php
@@ -38,7 +38,7 @@ if (isset($_POST) && $_POST) {
   </div>
   <div class="form-group">
   <label for="prix">Prix :</label>
-  <input type="text" class="form-control" onkeypress='return event.charCode >= 48 && event.charCode <= 57' id="prix" placeholder="Enter prix" aria-describedby="prixHelp" placeholder="Enter prix" name="prix" required>
+  <input type="text" class="form-control" onkeypress='return charCode != 46 && charCode > 31 && (charCode < 48 || charCode > 57' id="prix" placeholder="Enter prix" aria-describedby="prixHelp" placeholder="Enter prix" name="prix" required>
   </div>
   <div class="form-group">
   <label for="image" >Select image :</label>
