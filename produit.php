e841103a74ee9c0bf5cad0cff26f418d1d380a27
abdesslemdhaker <abdesslemdhakerr@gmail.com>
2023-11-29 14:18:24 +0100

    add css

--- produit.php
+++ produit.php
@@ -1,3 +1,30 @@
+<?php
+session_start();
+error_reporting(0);
+ini_set("display_errors", 0);
+include "./assets/database/db.php";
+
+if ((isset($_GET['produit']) && $_GET['produit'])) {
+    $sql = "SELECT * FROM produits WHERE description LIKE '%".$_GET['keyword']."%' OR name LIKE '%".$_GET['keyword']."%' AND categorie LIKE '%".$_GET['category']."%'";
+    $result = mysqli_query($db, $sql);
+}else{
+    header("Location: /");
+    die();
+}
+
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+  <title>
+      Campi.tn - the most comprehensive resource for beautiful private campsites.
+    </title>
+    <link href="./assets/css/style2.css" rel="stylesheet">
+</head>
+<body>
+<?php include "./navbar.php";?>
 <section class="py-5">
             <div class="container px-4 px-lg-5 my-5">
                 <div class="row gx-4 gx-lg-5 align-items-center">
@@ -21,25 +48,19 @@
                 </div>
             </div>
         </section>
-        <!-- Related items section-->
         <section class="py-5 bg-light">
             <div class="container px-4 px-lg-5 mt-5">
                 <h2 class="fw-bolder mb-4">Related products</h2>
                 <div class="row gx-4 gx-lg-5 row-cols-2 row-cols-md-3 row-cols-xl-4 justify-content-center">
                     <div class="col mb-5">
                         <div class="card h-100">
-                            <!-- Product image-->
                             <img class="card-img-top" src="https://dummyimage.com/450x300/dee2e6/6c757d.jpg" alt="..." />
-                            <!-- Product details-->
                             <div class="card-body p-4">
                                 <div class="text-center">
-                                    <!-- Product name-->
                                     <h5 class="fw-bolder">Fancy Product</h5>
-                                    <!-- Product price-->
                                     $40.00 - $80.00
                                 </div>
                             </div>
-                            <!-- Product actions-->
                             <div class="card-footer p-4 pt-0 border-top-0 bg-transparent">
                                 <div class="text-center"><a class="btn btn-outline-dark mt-auto" href="#">View options</a></div>
                             </div>
