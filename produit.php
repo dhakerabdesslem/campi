9ca0f794bd0db1060488894770ffc705502e4f1f
abdesslemdhaker <abdesslemdhakerr@gmail.com>
2023-12-05 17:37:57 +0100

    fix

--- produit.php
+++ produit.php
@@ -12,7 +12,7 @@ if ((isset($_GET['produit']) && $_GET['produit'])) {
     if (mysqli_num_rows($result) < 1) {
         header("Location: /");
     }
-    $sqlcat = "SELECT TOP 4 * FROM produits where categorie=".$category." AND id!=".$id_produit;
+    $sqlcat = "SELECT  * FROM produits where categorie=".$category." AND id!=".$id_produit;
     $resultcat = mysqli_query($db, $sqlcat);
 }else{
     header("Location: /");
