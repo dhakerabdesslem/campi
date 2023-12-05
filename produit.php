781af68938d6c5ddf918e432e0d576924d4488be
abdesslemdhaker <abdesslemdhakerr@gmail.com>
2023-12-05 18:48:22 +0100

    fix err

--- produit.php
+++ produit.php
@@ -19,6 +19,79 @@ if ((isset($_GET['produit']) && $_GET['produit'])) {
 }
 ?>
 <?php include "./navbar.php";?>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
+<link href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css" rel="stylesheet">
+<style>
+
+div.stars {
+
+  width: 270px;
+
+  display: inline-block;
+
+}
+
+ .mt-200{
+     margin-top:200px;  
+ }
+
+input.star { display: none; }
+
+ 
+
+label.star {
+
+  float: right;
+
+  padding: 10px;
+
+  font-size: 36px;
+
+  color: #4A148C;
+
+  transition: all .2s;
+
+}
+
+ 
+
+input.star:checked ~ label.star:before {
+
+  content: '\f005';
+
+  color: #FD4;
+
+  transition: all .25s;
+
+}
+
+ 
+input.star-5:checked ~ label.star:before {
+
+  color: #FE7;
+
+  text-shadow: 0 0 20px #952;
+
+}
+
+ 
+
+input.star-1:checked ~ label.star:before { color: #F62; }
+
+    
+
+label.star:hover { transform: rotate(-15deg) scale(1.3); }
+
+ 
+
+label.star:before {
+
+  content: '\f006';
+
+  font-family: FontAwesome;
+
+}
+</style>
 <section class="py-5">
             <div class="container px-4 px-lg-5 my-5">
                 <div class="row gx-4 gx-lg-5 align-items-center">
@@ -31,6 +104,49 @@ if ((isset($_GET['produit']) && $_GET['produit'])) {
                             <div class="badge bg-dark text-white position-absolute" style="top: 0.5rem; right: 0.5rem">Sale</div>
                             <?php } ?>
                         <h1 class="display-5 fw-bolder"><?= $res["name"] ?></h1>
+                        <div class="container d-flex justify-content-center mt-200">
+	
+
+<div class="row">
+
+	<div class="col-md-12">
+
+		<div class="stars">
+
+  <form action="">
+
+    <input class="star star-5" id="star-5" type="radio" name="star"/>
+
+    <label class="star star-5" for="star-5"></label>
+
+    <input class="star star-4" id="star-4" type="radio" name="star"/>
+
+    <label class="star star-4" for="star-4"></label>
+
+    <input class="star star-3" id="star-3" type="radio" name="star"/>
+
+    <label class="star star-3" for="star-3"></label>
+
+    <input class="star star-2" id="star-2" type="radio" name="star"/>
+
+    <label class="star star-2" for="star-2"></label>
+
+    <input class="star star-1" id="star-1" type="radio" name="star"/>
+
+    <label class="star star-1" for="star-1"></label>
+
+  </form>
+
+</div>
+
+
+		
+	</div>
+	
+
+</div>
+
+</div>
                         <div class="fs-5 mb-5">
                         <?php if ($res['sale'] != 0) { ?>
                             <span class="text-decoration-line-through"><?= $res['prix']?>TND</span>
