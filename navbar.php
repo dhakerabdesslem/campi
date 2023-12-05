9d40cea855c68198098deeac0add1b1bbdefb408
abdesslemdhaker <abdesslemdhakerr@gmail.com>
2023-12-05 16:49:09 +0100

    fix

--- navbar.php
+++ navbar.php
@@ -20,7 +20,7 @@
             <div class="container-fluid">
                 <div class="row">
                     <div class="col-md-2 my-auto d-none d-sm-none d-md-block d-lg-block">
-                        <h5 class="brand-name" href="/">Campi.tn</h5>
+                        <h5 class="brand-name" ><a href="/">Campi.tn</a></h5>
                     </div>
                     <div class="col-md-5 my-auto">
                         <form role="search">
@@ -74,7 +74,7 @@
         </div>
         <nav class="navbar navbar-expand-lg">
             <div class="container-fluid">
-                <a class="navbar-brand d-block d-sm-block d-md-none d-lg-none" href="#">
+                <a class="navbar-brand d-block d-sm-block d-md-none d-lg-none" href="/">
                 Campi.tn
                 </a>
                 <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
