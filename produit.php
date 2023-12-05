cf698d415f185d03db7e7f7ef98bf4092787ef85
abdesslemdhaker <abdesslemdhakerr@gmail.com>
2023-12-05 18:23:21 +0100

    errror

--- produit.php
+++ produit.php
@@ -21,31 +21,7 @@ if ((isset($_GET['produit']) && $_GET['produit'])) {
 <?php include "./navbar.php";?>
 <link href="./assets/css3/reviews.css" rel="stylesheet">
 <link href='https://unpkg.com/boxicons@2.1.1/css/boxicons.min.css' rel='stylesheet'>
-<script>
-    const allStar = document.querySelectorAll(".rating .star");
-const ratingValue = document.querySelector(".rating input");
 
-allStar.forEach((item, idx) => {
-  item.addEventListener("click", function () {
-    let click = 0;
-    ratingValue.value = idx + 1;
-
-    allStar.forEach((i) => {
-      i.classList.replace("bxs-star", "bx-star");
-      i.classList.remove("active");
-    });
-    for (let i = 0; i < allStar.length; i++) {
-      if (i <= idx) {
-        allStar[i].classList.replace("bx-star", "bxs-star");
-        allStar[i].classList.add("active");
-      } else {
-        allStar[i].style.setProperty("--i", click);
-        click++;
-      }
-    }
-  });
-});
-</script>
 <section class="py-5">
             <div class="container px-4 px-lg-5 my-5">
                 <div class="row gx-4 gx-lg-5 align-items-center">
@@ -75,7 +51,32 @@ allStar.forEach((item, idx) => {
                             </button>
                         </div>
                     </div>
-                    <div class="wrapper" >
+                    <script>
+const allStar = document.querySelectorAll(".rating .star");
+const ratingValue = document.querySelector(".rating input");
+
+allStar.forEach((item, idx) => {
+  item.addEventListener("click", function () {
+    let click = 0;
+    ratingValue.value = idx + 1;
+
+    allStar.forEach((i) => {
+      i.classList.replace("bxs-star", "bx-star");
+      i.classList.remove("active");
+    });
+    for (let i = 0; i < allStar.length; i++) {
+      if (i <= idx) {
+        allStar[i].classList.replace("bx-star", "bxs-star");
+        allStar[i].classList.add("active");
+      } else {
+        allStar[i].style.setProperty("--i", click);
+        click++;
+      }
+    }
+  });
+});
+</script>
+    <div class="wrapper" >
 		<h3>AVIS CLIENTS</h3>
 		<form action="#">
 			<div class="rating">
