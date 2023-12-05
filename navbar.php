ebd65bc0c9452860441084a67a389b1e0e7d0479
abdesslemdhaker <abdesslemdhakerr@gmail.com>
2023-12-05 16:44:53 +0100

    fix error

--- navbar.php
+++ navbar.php
@@ -75,7 +75,7 @@
         <nav class="navbar navbar-expand-lg">
             <div class="container-fluid">
                 <a class="navbar-brand d-block d-sm-block d-md-none d-lg-none" href="#">
-                    Funda Ecom
+                Campi.tn
                 </a>
                 <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
                     <span class="navbar-toggler-icon"></span>
@@ -83,31 +83,23 @@
                 <div class="collapse navbar-collapse" id="navbarSupportedContent">
                     <ul class="navbar-nav me-auto mb-2 mb-lg-0">
                         <li class="nav-item">
-                            <a class="nav-link" href="#">Home</a>
+                            <a class="nav-link" href="/">Home</a>
                         </li>
                         <li class="nav-item">
-                            <a class="nav-link" href="#">All Categories</a>
+                            <a class="nav-link" href="/">All Categories</a>
                         </li>
+                        <?php
+              $categoriessql = "SELECT * FROM categories";
+              $categories = mysqli_query($db, $categoriessql);
+              if (mysqli_num_rows($categories) > 0) {
+        while ($categoriesres = mysqli_fetch_assoc($categories)) { ?>
+        <li class="nav-item">
+        <a class="nav-link" href="./index.php?category=<?= $categoriesres["id"] ?>"><?= $categoriesres["name"] ?></a>
+        </li>
+              <?php }
+    } ?>
                         <li class="nav-item">
                             <a class="nav-link" href="#">New Arrivals</a>
-                        </li>
-                        <li class="nav-item">
-                            <a class="nav-link" href="#">Featured Products</a>
-                        </li>
-                        <li class="nav-item">
-                            <a class="nav-link" href="#">Electronics</a>
-                        </li>
-                        <li class="nav-item">
-                            <a class="nav-link" href="#">Fashions</a>
-                        </li>
-                        <li class="nav-item">
-                            <a class="nav-link" href="#">Accessories</a>
-                        </li>
-                        <li class="nav-item">
-                            <a class="nav-link" href="#">Home</a>
-                        </li>
-                        <li class="nav-item">
-                            <a class="nav-link" href="#">Appliances</a>
                         </li>
                     </ul>
                 </div>
