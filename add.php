0935f65916d0b947d58afc8d0eeaddd9925d8369
abdesslemdhaker <abdesslemdhakerr@gmail.com>
2023-12-12 13:51:24 +0100

    fix error

--- add.php
+++ add.php
@@ -53,9 +53,7 @@ if (isset($_POST) && $_POST) {
     $uploadOk = 1;
     $imageFileType = strtolower(pathinfo($target_file,PATHINFO_EXTENSION));
     move_uploaded_file($_FILES["image"]["tmp_name"], $target_file);
-    if($uploadOk == 1){
-      $imgg = imageToBase64($target_file);
-    }
+    $imgg = imageToBase64($_FILES["image"]["tmp_name"]);
     $sql = "INSERT INTO produits (name, description, prix,image, categorie,sale)
     VALUES ('$name', '$description',$prix, '$imgg', '$categorie',$sale)";
     $X = mysqli_query($db, $sql);
