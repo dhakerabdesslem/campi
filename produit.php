b5737f1ed0cd3f26fe2f0200f8e9550ff711e276
abdesslemdhaker <abdesslemdhakerr@gmail.com>
2023-12-13 13:25:41 +0100

    fix error

--- produit.php
+++ produit.php
@@ -105,8 +105,8 @@ if ((isset($_GET['produit']) && $_GET['produit'])) {
                     </div>
                     <?php }?>
     </div>
-    <div class="nav nav-tabs mb-3">
-                                    <button class="nav-link border-white border-bottom-0" type="button" role="tab" id="nav-mission-tab" data-bs-toggle="tab" data-bs-target="#nav-mission" aria-controls="nav-mission" aria-selected="false">Reviews</button>
+                                    <div class="nav nav-tabs mb-3">
+                                    <button class="nav-link active border-white border-bottom-0" type="button" role="tab" id="nav-mission-tab" data-bs-toggle="tab" data-bs-target="#nav-mission" aria-controls="nav-mission" aria-selected="true">Reviews</button>
                                 </div>
     <div class="tab-pane" id="nav-mission" role="tabpanel" aria-labelledby="nav-mission-tab">
                                     <?php 
@@ -146,7 +146,8 @@ if ((isset($_GET['produit']) && $_GET['produit'])) {
                                 <div class="col-lg-12">
                                     <div class="d-flex justify-content-between py-3 mb-5">
                                         <div class="d-flex align-items-center">
-                                            <p class="mb-0 me-3">Please rate:</p><br>
+                                            <p class="mb-0 me-3">Please rate:</p>
+                                </br>
                                             <div class="rating">
   <input type="radio" id="star5" name="rating" value="5" />
   <label class="star" for="star5" title="Awesome" aria-hidden="true"></label>
